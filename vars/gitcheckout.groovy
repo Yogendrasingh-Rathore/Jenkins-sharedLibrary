a998844d3d3664dcf251630578030effc895fa19
Yogendrasingh-Rathore <53259324+Yogendrasingh-Rathore@users.noreply.github.com>
2019-11-27 20:12:24 +0530

    Added hello

--- vars/gitcheckout.groovy
+++
@@ -1,8 +0,0 @@
-def call(Map stageParams) {
-   checkout(
-   [$class: 'GitSCM', 
-    branches: [[name: '*/master']],
-    doGenerateSubmoduleConfigurations: false,
-    userRemoteConfigs: [[url: 'https://github.com/Yogendrasingh-Rathore/Jenkins-sharedLibrary.git']]]
-   )
-}
